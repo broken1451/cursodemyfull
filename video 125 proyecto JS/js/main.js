ec4037d6d2c8c68576f9838efa4d27444b29ef97
adrian <bravo>
2019-01-26 14:10:16 -0430

    actualizacion proyecto

--- video 125 proyecto JS/js/main.js
+++ video 125 proyecto JS/js/main.js
@@ -1,57 +1,69 @@
 /*Js principal*/
 
-//Slider
+
+
+
 $(document).ready(function () {
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: false,
-        slideWidth: 1200,
-        pager:false,
+    
+if (window.location.href.indexOf("index") > -1) {
+        //Slider
+    
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: false,
+            slideWidth: 1200,
+            pager:false,
+    });
+    
+}  
+
+
+    
+if (window.location.href.indexOf("index") > -1) {  
+    //Post
+    var posts = [
+        {title:"Prueba de titulo",date: "Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
+        {title:"Prueba de titulo 2",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
+        {title:"Prueba de titulo 3",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
+        {title:"Prueba de titulo 4",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
+        {title:"Prueba de titulo 5",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"}];
+        
+        
+                /*
+                    posts.forEach((elemento,indice) => {
+           
+                        var post =`
+                        <article class="post">
+                        <h2>${elemento.title}</h2>
+                        <span class="date">${elemento.date}</span>
+                        <p>${elemento.content}</p>
+                        <a href="#" class="buton-more">Leer mas</a>
+                    </article>
+                        `; 
+    
+                        $("#post").append(post);
+                        console.log(post);
+        
+                    });
+       
+                */
+    
+      posts.map((elemento,i) =>{
+        var post =`
+        <article class="post">
+        <h2>${elemento.title}</h2>
+        <span class="date">${elemento.date}</span>
+        <p>${elemento.content}</p>
+        <a href="#" class="buton-more">Leer mas</a>
+        </article>
+        `; 
+    
+        $("#post").append(post);
+        console.log(post);
+    
       });
 
-//Post
-var posts = [
-    {title:"Prueba de titulo",date: "Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
-    {title:"Prueba de titulo 2",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
-    {title:"Prueba de titulo 3",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
-    {title:"Prueba de titulo 4",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"},
-    {title:"Prueba de titulo 5",date:"Publicado:" + moment().format("MMMM Do YYYY, h:mm:ss a"),content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis eius facere sequi rerum aperiam. Magni maxime modi placeat possimus dicta, et, minus natus itaque, pariatur similique porro aliquid repellat.Lorem ipsum dolor sit amet consectetur adipisicing elitEum officiis eius facere sequi rerum aperiam. Magnmaxime modi placeat possimus dicta, et, minus natuitaque, pariatur similique porro aliquid repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit molestiae in nihil rem, commodi voluptatibus, officiis ab voluptatum esse quia repudiandae! Molestiae nulla sequi unde saepe minus fuga atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reprehenderit harum, delectus quidem unde ab, obcaecati ratione autem facilis iure odio eius voluptas temporibus veniam? Dolores architecto rem fuga accusantium!"}];
-    
-    
-            /*
-                posts.forEach((elemento,indice) => {
-       
-                    var post =`
-                    <article class="post">
-                    <h2>${elemento.title}</h2>
-                    <span class="date">${elemento.date}</span>
-                    <p>${elemento.content}</p>
-                    <a href="#" class="buton-more">Leer mas</a>
-                </article>
-                    `; 
-
-                    $("#post").append(post);
-                    console.log(post);
-    
-                });
-   
-            */
-
-  posts.map((elemento,i) =>{
-    var post =`
-    <article class="post">
-    <h2>${elemento.title}</h2>
-    <span class="date">${elemento.date}</span>
-    <p>${elemento.content}</p>
-    <a href="#" class="buton-more">Leer mas</a>
-    </article>
-    `; 
-
-    $("#post").append(post);
-    console.log(post);
-
-  });
-
+}
 
 
 
@@ -103,7 +115,7 @@ var variableNombre = localStorage.getItem("name");
 
 if (variableNombre != null && variableNombre != undefined && variableNombre != " ") {
 
-    $("#about p").html(" <br> Welcome " + variableNombre );
+    $("#about p").html(" <br> <strong> Welcome " + variableNombre + "</strong>");
     $("#about p").append("<a href='#' id='logout'> Cerrar sesion </a>");
     $("#login").hide();
     $("#logout").click(function () { 
@@ -115,5 +127,31 @@ if (variableNombre != null && variableNombre != undefined && variableNombre != "
 
 console.log(variableNombre);
 console.log(posts);
+
+
+
+if (window.location.href.indexOf("about") > -1){
+    //Acordeon
+    $("#acordeon").accordion();
+
+
+}
+
+
+if (window.location.href.indexOf("reloj") > -1){
+    
+//Reloj
+   setInterval(function () { 
+    var reloj = moment().format("hh:mm:ss a");
+    $("#reloj").html(reloj);
+    },1000)
+ 
+
+   console.log(reloj);
+
+
+}
+
+
 });
 
